b34860f658ea593f633fe1afd33c2f1e4744262c
Ceres Barros <ceres.barros@nrcan-rncan.gc.ca>
2025-09-24 11:18:31 -0700

    `input` data documentation

---
+++ R/input.R
@@ -0,0 +1,17 @@
+#' Default/Example input data for `con.pyro`
+#'
+#' TODO DESCRIPTION
+#'
+#' @format A `data.frame` with columns:
+#' \describe{
+#'   \item{id}{TODO DESCRIPTION}
+#'   \item{season}{TODO DESCRIPTION}
+#'   \item{density}{TODO DESCRIPTION}
+#'   \item{stand}{TODO DESCRIPTION}
+#'   \item{sfc}{TODO DESCRIPTION}
+#'   \item{cbd}{TODO DESCRIPTION}
+#'   \item{smooth_cfi}{TODO DESCRIPTION}
+#'   \item{XXX}{optional. TODO DESCRIPTION}
+#'   \item{XXX}{optional. TODO DESCRIPTION}
+#'   \item{XXX}{optional. TODO DESCRIPTION}
+#' }
