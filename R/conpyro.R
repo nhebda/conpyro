93631070ea34b47f985788cd166c6a0ca5368710
Ceres Barros <ceres.barros@nrcan-rncan.gc.ca>
2025-09-24 11:24:52 -0700

    mv a tool_* fn to separate script

--- R/conpyro.R
+++ R/conpyro.R
@@ -492,17 +492,6 @@ conpyro <- function(
 }
 
 # pCFO calculator
-tool_pcfo <- function(model = 11, mc = 7.8, ws = 20, fsg = 9.5, sfc = 1.8) {
-  assert_choice(model, c(7, 8, 10, 11))
-  assert_number(mc, lower = 5, upper = 20)
-  assert_number(ws, lower = 0, upper = 60)
-  assert_number(fsg, lower = 0.5, upper = 20)
-  assert_number(sfc, lower = 0.1, upper = 6)
-  pcfo <- fn_pcfo(model, ws, fsg, sfc, mc)
-  out  <- list("pcfo" = pcfo)
-  out
-}
-
 # mc calculator
 tool_mc <- function(
     ffmc    = 91.2,
