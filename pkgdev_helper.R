32232c0b826b93cda0561b75ad41bd8598c67ea4
Ceres Barros <ceres.barros@nrcan-rncan.gc.ca>
2025-09-24 15:55:16 -0700

    devtools::load_all note

--- pkgdev_helper.R
+++ pkgdev_helper.R
@@ -11,5 +11,7 @@ devtools::check()
 
 devtools::test()
 
+devtools::load_all() ## useful to load your package wihtout installing (basically sources all your funcitons, including internal ones)
+
 ## Online documentation with pkgdown (funtion calls to come when apprpriate)
 # install.packages("pkgdown")
