28e8bcfe9d09be316c87e1d8282821abe1d1da1a
nhebda <nhebda@gmail.com>
2025-10-29 22:56:36 -0700

    Add unit tests for fn_MCFFMC(), fn_MCDMC(), fn_MCSA_idx()

--- tests/testthat/test-conpyro.R
+++ tests/testthat/test-conpyro.R
@@ -3,3 +3,17 @@ test_that("MCFFMC calc works", {
   expect_equal(fn_MCFFMC(90), 10.8307692308)
   expect_identical(fn_MCFFMC(101), 0)
 })
+
+test_that("MCDMC calc works", {
+  expect_equal(fn_MCDMC(0), 300.0070115918)
+  expect_equal(fn_MCDMC(100), 48.0015203601)
+  expect_equal(fn_MCDMC(200), 22.8002339585)
+})
+
+test_that("MCSA_idx calc works", {
+  expect_identical(fn_MCSA_idx("spring", "light", "deciduous"), 111)
+  expect_identical(fn_MCSA_idx("summer", "moderate", "douglas-fir"), 222)
+  expect_identical(fn_MCSA_idx("fall", "dense", "mixedwood"), 333)
+  expect_identical(fn_MCSA_idx("sp-su", "light", "pine"), 414)
+  expect_identical(fn_MCSA_idx("summer", "moderate", "spruce"), 225)
+})
